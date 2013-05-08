9848b8996200f8694625920cf89363d5778afcca
Anton Osten <anton@ostensible.me>
2013-05-08 01:19:20 -0500

    raw output option

--- run_reader.py
+++ run_reader.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3.3
 
+import argparse, pprint
 from joystickreader import JoystickReader, BadInputError
 
 def main():
     # initialise the serial port
     print('Initialising the serial port...')
     jr = JoystickReader()
+    
+    pp = pprint.PrettyPrinter()
     
     print('Waiting for input.')
     
@@ -26,28 +29,35 @@ def main():
             if last_values['x_steps'] != values['x_steps'] or last_values['y_steps'] != values['y_steps']:
                 last_values = values
                 
-                button_pressed = values['button_pressed']
+                if args.v:
+                    pp.pprint(values)
+                else:
+                    button_pressed = values['button_pressed']
                 
-                y_direction = values['y_direction']
-                y_steps = values['y_steps']
+                    y_direction = values['y_direction']
+                    y_steps = values['y_steps']
             
-                x_direction = values['x_direction']
-                x_steps = values['x_steps']
+                    x_direction = values['x_direction']
+                    x_steps = values['x_steps']
                 
-                if button_pressed:
-                    print('You pressed the button.')
-                if y_steps > 0:
-                    print('Y is {} steps {}'.format(values['y_steps'],
-                        values['y_direction']))
-                if x_steps > 0:
-                    print('X is {} steps {}'.format(values['x_steps'],
-                        values['x_direction']))
-                if not button_pressed and y_steps is 0 and x_steps is 0:    
-                    print('All at rest')
+                
+                    if button_pressed:
+                        print('You pressed the button.')
+                    if y_steps > 0:
+                        print('Y is {} steps {}'.format(values['y_steps'],
+                            values['y_direction']))
+                    if x_steps > 0:
+                        print('X is {} steps {}'.format(values['x_steps'],
+                            values['x_direction']))
+                    if not button_pressed and y_steps is 0 and x_steps is 0:    
+                        print('All at rest')
             else:
                 continue
             
     
 
 if __name__ == '__main__':
+    argp = argparse.ArgumentParser()
+    argp.add_argument('-v', action='store_true', default=False, help='verbose mode')
+    args = argp.parse_args()
     main()
